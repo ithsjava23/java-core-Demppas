4ec51ba6c8e73ef9590ae9863b5e5fd54d7df963
demppas <dennis.staffansson@iths.se>
2023-10-18 14:58:31 +0200

    Inplemented methods for exceptions

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -4,9 +4,9 @@ import java.util.Objects;
 
 public class ProductRecord {
    private String Name;
-    private int prodID;
+    private String prodID;
     private String Category;
-    private Double Price;
+    public Double Price;
 
     ProductRecord(String Name, int prodID, String Category, Double Price) {
         this.Name = Name;
@@ -15,45 +15,22 @@ public class ProductRecord {
         this.Price = Price;
     }
 
-    public String getName() {
+    public static String getName() {
         return Name;
     }
 
-    public int getprodID() {
+    public static String getProdID() {
         return prodID;
     }
 
-    public String getCategory() {
+    public static String getCategory() {
         return Category;
     }
 
-    public Double getPrice() {
+    public static Double getPrice() {
         return Price;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (ProductRecord) obj;
-        return Objects.equals(this.Name, that.Name) &&
-                this.prodID == that.prodID &&
-                Objects.equals(this.Category, that.Category) &&
-                Objects.equals(this.Price, that.Price);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(Name, prodID, Category, Price);
-    }
-
-    @Override
-    public String toString() {
-        return "ProductRecord[" +
-                "Name=" + Name + ", " +
-                "prodID=" + prodID + ", " +
-                "Category=" + Category + ", " +
-                "Price=" + Price + ']';
-    }
 
 }
--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -9,13 +9,39 @@ public class Warehouse {
     public static Warehouse getInstance() {
         Warehouse warehouse = new Warehouse();
         return warehouse;
+
     }
 
-    public void addProduct (ProductRecord productRecord){
+    public void equals(Warehouse warehouse) {
+            if (new Warehouse.equals(warehouse)) {
+                warehouse = new Warehouse;
+            }
+    }
+
+    public void addProduct(ProductRecord productRecord) {
         productList.add(productRecord);
     }
 
+    public void tryName (ProductRecord productrecord) {
+        if (ProductRecord.getName().isEmpty() || ProductRecord.getName() = "") {
+            throw new IllegalArgumentException("Product name can't be null or empty.");
+        }
+    }
+    public void tryCategory (ProductRecord productrecord){
+        if (ProductRecord.getCategory().isEmpty()) {
+            throw new IllegalArgumentException("Category can't be null.");
+        }
+    }
+
+    public void checkID (ProductRecord productRecord){
+        if (ProductRecord.getProdID().equals("") || ProductRecord.getProdID().isEmpty()){
+            //assign UUID to ProdID
+        }
+    }
+
+    public void tryPrice (ProductRecord productrecord){
+        if (ProductRecord.getPrice().isNaN())
+            ProductRecord.Price = 0;
+    }
 
 }
-
-
